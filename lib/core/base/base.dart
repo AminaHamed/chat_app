316a7c4fd5d08f46fbb2b7cfe885fdff7cb9bdc5
Amina Hamed <61025388+AminaHamed@users.noreply.github.com>
2024-06-04 15:21:25 +0300

    Update base.dart

--- lib/core/base/base.dart
+++ lib/core/base/base.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../utils/dialog_utils.dart';
+
 abstract class BaseNavigator {
   void showLoading();
 
@@ -5,3 +9,42 @@ abstract class BaseNavigator {
 
   void hideLoading();
 }
+
+abstract class BaseViewModel<N extends BaseNavigator>extends ChangeNotifier {
+N? navigator;
+}
+
+abstract class BaseState<T extends StatefulWidget,vm extends BaseViewModel> extends State<T> implements BaseNavigator {
+late vm viewModel;
+  @override
+  void initState() {
+    super.initState();
+    viewModel = initViewModel();
+    viewModel.navigator = this;
+
+  }
+
+  vm initViewModel(); 
+
+  
+
+  @override
+  void hideLoading() {
+    DialogUtils.hideDialog(context);
+  }
+
+  @override
+  void showLoading() {
+    DialogUtils.showProgressDialog(context, 'Loading');
+  }
+
+  @override
+  void showMessage(String message) {
+    DialogUtils.showMessage(context, message);
+  }
+
+
+
+  
+}
+
